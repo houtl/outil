6f1dff693364eeec7aee92b4e560b45d170828c5
thou <houtianle@gmail.com>
2017-06-28 17:16:40 +0200

    2017年 06月 28日 星期三 17:16:40 CEST

--- addssh.sh
+++ addssh.sh
@@ -3,15 +3,22 @@
 red="\033[0;31m"
 green="\033[0;32m"
 nocolor="\033[0m"
-echo $HOME 
+
 usage() {
 	echo "$green usage: addssh [User mail] [key文件名(prive, new, 等)]$nocolor"
 	exit
 }
+
 if [ $# != 2 ]; then
 	usage
 fi
+
 echo "$HOME/.ssh/id_rsa_$2" | ssh-keygen -P passphrase -t rsa -C $1
+
+if [ $SESSION="ubuntu" ]; then
+	cat $HOME/.ssh/id_rsa_prive.pub | xclip -selection clipboard
+fi
+
 echo "# default
 Host github.com
 HostName github.com
@@ -22,3 +29,6 @@ Host gp.github.com
 HostName github.com
 User git
 IdentityFile ~/.ssh/id_rsa_prive" >> ~/.ssh/config
+
+git config --global --unset user.name
+git config --global --unset user.email
--- settime.sh
+++ settime.sh
@@ -1,2 +1,15 @@
+<<<<<<< HEAD
+red="\033[0;31m"
+green="\033[0;32m"
+nocolor="\033[0m"
+
+if [ -z "$1" ]; then
+	echo $red
+	echo "usage: settime [0(as linux) 或 1(as windows)]"
+	echo $nocolor
+	exit
+fi
+=======
+>>>>>>> 20e1404e8fd2cb2e6f9d2f9e6e96157b9bd6a34f
 sudo timedatectl set-local-rtc 1
 timedatectl
