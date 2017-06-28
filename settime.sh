2b311a34ad8151ce8ba2705ea5fa2b5b1ac95439
thou <houtianle@gmail.com>
2017-06-28 23:49:13 +0200

    2017年 06月 28日 星期三 23:49:13 CEST

--- settime.sh
+++ settime.sh
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 red="\033[0;31m"
 green="\033[0;32m"
 nocolor="\033[0m"
 
 if [ -z "$1" ]; then
 	echo $red
-	echo "usage: settime [0(as linux) 或 1(as windows)]"
+	echo "usage: settime [0(linux默认设置) 或 1(windows默认设置)]"
 	echo $nocolor
 	exit
 fi
-=======
->>>>>>> 20e1404e8fd2cb2e6f9d2f9e6e96157b9bd6a34f
+
 sudo timedatectl set-local-rtc 1
 timedatectl
