6f74d191febc82564613371c4621e810bd7f2382
thou <thou@student.42.fr>
2018-02-04 10:58:15 +0000

    dimanche 4 février 2018, 10:58:15 (UTC+0000)

--- cplft.sh
+++ cplft.sh
@@ -18,15 +18,17 @@ if [ /home/thou = $(pwd) ]; then
 	echo "libft se trouve ici"
 else
 	if [ -z "$1" ]; then
+		PATH_LIBFT=$(pwd)
 		echo "没有参数，拷贝libft到当前文件夹"
 		rm -rf libft
 		cp -R ~/libft ./
 	else
+		PATH_LIBFT=$(pwd)$1
 		echo "拷贝libft到$1文件夹"
 		rm -rf $1/libft
 		cp -R ~/libft $1
-
+	fi
+	rm -rf $PATH_LIBFT/libft/.git
+	rm -rf $PATH_LIBFT/libft/.travis.yml
+	rm -rf $PATH_LIBFT/libft/auteur
 fi
-rm -rf $1libft/.git
-rm $1libft/.travis.yml
-rm $1libft/auteur
